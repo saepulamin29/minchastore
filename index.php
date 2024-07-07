87ad1eb7042a8e27ca09044d731128985e03329d
saepulamin29 <saepulamien29@gmail.com>
2024-07-07 18:06:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mincha Store</title>
+</head>
+
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    body {
+        background-image: url('images/Background/wall.jpg');
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        text-align: center;
+    }
+
+    .container {
+        background-color: #ffffff;
+        padding: 18px 20px;
+        border-radius: 20px;
+    }
+
+    .container a {
+        text-decoration: none;
+        color: #0ea5e9;
+        transition-duration: 300ms;
+        margin-top: 18px;
+    }
+
+    .container a:hover {
+        text-decoration: underline;
+        color: #0369a1;
+    }
+</style>
+
+<body>
+    <div class="container">
+        <h1>Selamat datang di Mincha Store</h1>
+        <a href="login.php">Silahkan login terlebih dahulu</a>
+    </div>
+</body>
+
+</html>
---
+++ logout.php
@@ -0,0 +1,5 @@
+<?php
+// Menghapus session yang telah dibuat
+session_start();
+session_destroy();
+header('location: login.php');
